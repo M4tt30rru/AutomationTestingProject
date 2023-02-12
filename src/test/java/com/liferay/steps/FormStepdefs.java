2a6fe99431379a50be0cf794272abaa04c5fc9a5
Matteo Orru <matteo.orru@lastminute.com>
2023-02-12 10:55:47 +0100

    refactor to page object 1 - extract methods

--- src/test/java/com/liferay/steps/FormStepdefs.java
+++ src/test/java/com/liferay/steps/FormStepdefs.java
@@ -22,17 +22,12 @@ public class FormStepdefs {
 
     @Given("A user landing on the form page")
     public void aUserLandingOnTheFormPage() {
-        webDriver.navigate().to("https://forms.liferay.com/web/forms/shared/-/form/122548");
-    }
-
-    @When("the user fills in the {string} input")
-    public void theUserFillsInTheFootballPlayerInput(String input) throws InterruptedException {
-
+        getTo();
     }
 
     @And("the user clicks on the submit button")
     public void theUserClicksOnTheSubmitButton() {
-        webDriver.findElement(By.xpath("//button[@type='submit' and contains(text(),'Submit')]")).click();
+        clickOnSubmitButton();
     }
 
     @Then("A message showing the following {string} shows up")
@@ -47,18 +42,34 @@ public class FormStepdefs {
 
     @When("the user fills {string} value in the football player input")
     public void theUserFillsFootballPlayerValueInTheFootballPlayerInput(String footBallPlayerName) throws InterruptedException {
-        // TODO temporary, to be change into a waiting condition
-        Thread.sleep(5000);
-        webDriver.findElement(By.xpath("//input[@dir='ltr' and contains(@class,'ddm-field-text')]")).sendKeys(footBallPlayerName);
+        fillsInTheFootballPlayerField(footBallPlayerName);
     }
 
     @And("the user fills {string} value in the LifeRay foundation input")
     public void theUserFillsLifeRayFoundationDateValueInTheLifeRayFoundationInput(String date) {
-        webDriver.findElement(By.xpath("//div[@class='date-picker']//input[@dir='ltr']")).sendKeys(date);
+        fillInTheFoundationDateField(date);
     }
 
     @And("the user fills {string} value in the career question input")
     public void theUserFillsTestFieldValueInTheCareerQuestionInput(String answer) {
         webDriver.findElement(By.xpath("//textarea")).sendKeys(answer);
+    }
+
+    private void getTo() {
+        webDriver.navigate().to("https://forms.liferay.com/web/forms/shared/-/form/122548");
+    }
+
+    private void clickOnSubmitButton() {
+        webDriver.findElement(By.xpath("//button[@type='submit' and contains(text(),'Submit')]")).click();
+    }
+
+    private void fillsInTheFootballPlayerField(String footBallPlayerName) throws InterruptedException {
+        // TODO temporary, to be change into a waiting condition
+        Thread.sleep(5000);
+        webDriver.findElement(By.xpath("//input[@dir='ltr' and contains(@class,'ddm-field-text')]")).sendKeys(footBallPlayerName);
+    }
+
+    private void fillInTheFoundationDateField(String date) {
+        webDriver.findElement(By.xpath("//div[@class='date-picker']//input[@dir='ltr']")).sendKeys(date);
     }
 }
