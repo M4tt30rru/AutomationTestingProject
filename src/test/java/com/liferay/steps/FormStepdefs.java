97e219741a2429fd1db0c277413f33bd8117f589
Matteo Orru <matteo.orru@lastminute.com>
2023-02-12 10:24:35 +0100

    segregating different inputs in separate stepdefs

--- src/test/java/com/liferay/steps/FormStepdefs.java
+++ src/test/java/com/liferay/steps/FormStepdefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class FormStepdefs {
@@ -23,7 +24,8 @@ public class FormStepdefs {
     }
 
     @When("the user fills in the {string} input")
-    public void theUserFillsInTheFootballPlayerInput(String input) {
+    public void theUserFillsInTheFootballPlayerInput(String input) throws InterruptedException {
+
     }
 
     @And("the user clicks on the submit button")
@@ -32,5 +34,19 @@ public class FormStepdefs {
 
     @Then("A message showing the following {string} shows up")
     public void aMessageShowingTheFollowingMessageShowsUp(String message) {
+    }
+
+    @When("the user fills {string} value in the football player input")
+    public void theUserFillsFootballPlayerValueInTheFootballPlayerInput(String footBallPlayerName) throws InterruptedException {
+        Thread.sleep(5000);
+        webDriver.findElement(By.xpath("//input[@dir='ltr' and contains(@class,'ddm-field-text')]")).sendKeys(footBallPlayerName);
+    }
+
+    @And("the user fills {string} value in the LifeRay foundation input")
+    public void theUserFillsLifeRayFoundationDateValueInTheLifeRayFoundationInput() {
+    }
+
+    @And("the user fills {string} value in the career question input")
+    public void theUserFillsTestFieldValueInTheCareerQuestionInput() {
     }
 }
--- src/test/resources/FormFeatureFile.feature
+++ src/test/resources/FormFeatureFile.feature
@@ -2,9 +2,9 @@ Feature: Liferay Form
 
   Scenario Outline: Filling in data in the form and submit
     Given A user landing on the form page
-    When the user fills in the <Football.Player> input
-    And the user fills in the <LifeRay.Foundation.Date> input
-    And the user fills in the <Test.Field> input
+    When the user fills <Football.Player> value in the football player input
+    And the user fills <LifeRay.Foundation.Date> value in the LifeRay foundation input
+    And the user fills <Test.Field> value in the career question input
     And the user clicks on the submit button
     Then A message showing the following <Message> shows up
 
